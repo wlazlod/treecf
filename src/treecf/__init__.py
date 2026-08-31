e8172e3595d0ba9363a9b70ff57fa90676c0b770
Daniel Wlazlo <wlazlo.daniel@gmail.com>
2026-08-31 03:52:21 +0200

    release: 0.3.0 version bump

--- src/treecf/__init__.py
+++ src/treecf/__init__.py
@@ -30,7 +30,7 @@ from treecf.plausibility import Plausibility
 from treecf.regions import RecourseRegion
 from treecf.targets import Target
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 
 __all__ = [
     "AllowMissing",
